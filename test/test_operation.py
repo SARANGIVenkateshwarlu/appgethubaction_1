cb8f13f843dc47b0ad1c910a48dba40706e646b0
Sarangi_Venkateshwarlu <venky.sarangi@gmail.com>
2026-01-28 14:35:51 +0800

    updated

--- test/test_operation.py
+++ test/test_operation.py
@@ -3,10 +3,11 @@ from src.math_operations import add,sub # functions imported
 def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
+    assert add(10,1)==11
 
 
 def test_sub():
-    assert add(5,3)==2
-    assert add(10,1)==9
-    assert add(6,3)==3
-    assert add(10,5)==5
+    assert sub(5,3)==2
+    assert sub(10,1)==9
+    assert sub(6,3)==3
+    assert sub(10,5)==5
